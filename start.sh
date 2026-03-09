765008e2cc28df609dd23b2379e8cabf16135e77
victorodia <victor.odia14@gmail.com>
2026-03-09 22:08:18 +0100

    Fix python command not found by switching to python3 explicitly

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd backend && python -m gunicorn main:app -w 1 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
+cd backend && python3 -m gunicorn main:app -w 1 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
